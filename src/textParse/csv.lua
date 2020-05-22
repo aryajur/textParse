3a15a31c176f1b89c2193371eba4cd1b2812205f
aryajur <milind.gupta@gmail.com>
2020-05-21 21:58:30 -0700

    csv reader module

---
+++ src/textParse/csv.lua
@@ -0,0 +1,62 @@
+-- Module for CSV parsing and associated functions
+
+
+local modname = ...
+
+local string = string
+local table = table
+
+local M = {} 
+package.loaded[modname] = M
+if setfenv then
+	setfenv(1,M)
+else
+	_ENV = M
+end
+
+_VERSION = "1.16.11.04"
+
+
+-- takes the csv line and the separator string (sep), default separator is ','
+-- it returns the table containing all the elements in the return table array
+function ParseCSVLine (line,sep) 
+	local res = {}
+	local pos = 1
+	sep = sep or ','
+	while true do 
+		local c = string.sub(line,pos,pos)
+		if (c == "") then break end
+		if (c == '"') then
+			-- quoted value (ignore separator within)
+			local txt = ""
+			repeat
+				local startp,endp = string.find(line,'^%b""',pos)
+				txt = txt..string.sub(line,startp+1,endp-1)
+				pos = endp + 1
+				c = string.sub(line,pos,pos) 
+				if (c == '"') then txt = txt..'"' end 
+				-- check first char AFTER quoted string, if it is another
+				-- quoted string without separator, then append it
+				-- this is the way to "escape" the quote char in a quote. example:
+				--   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+			until (c ~= '"')
+			table.insert(res,txt)
+			if not (c == sep or c == "") then
+				return nil
+			end
+			pos = pos + 1
+		else	
+			-- no quotes used, just look for the first separator
+			local startp,endp = string.find(line,sep,pos)
+			if (startp) then 
+				table.insert(res,string.sub(line,pos,startp-1))
+				pos = endp + 1
+			else
+				-- no separator found -> use rest of string and terminate
+				table.insert(res,string.sub(line,pos))
+				break
+			end 
+		end
+	end
+	return res
+end

3d8f383a584318e3bff2b545803d89920367cd90
aryajur <milind.gupta@gmail.com>
2021-04-22 13:10:06 -0700

    Line endings change

--- src/textParse/csv.lua
+++ src/textParse/csv.lua
@@ -45,6 +45,26 @@ function ParseCSVLine (line,sep)
 				return nil
 			end
 			pos = pos + 1
+		elseif (c == "'") then
+			-- quoted value (ignore separator within)
+			local txt = ""
+			repeat
+				local startp,endp = string.find(line,"^%b''",pos)
+				txt = txt..string.sub(line,startp+1,endp-1)
+				pos = endp + 1
+				c = string.sub(line,pos,pos) 
+				if (c == "'") then txt = txt.."'" end 
+				-- check first char AFTER quoted string, if it is another
+				-- quoted string without separator, then append it
+				-- this is the way to "escape" the quote char in a quote. example:
+				--   value1,'blub''blip''boing',value3  will result in blub'blip'boing  for the middle
+			until (c ~= "'")
+			table.insert(res,txt)
+			if not (c == sep or c == "") then
+				return nil
+			end
+			pos = pos + 1
+		
 		else	
 			-- no quotes used, just look for the first separator
 			local startp,endp = string.find(line,sep,pos)
